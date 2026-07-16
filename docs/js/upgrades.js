11a4a7a4de376b36bdb435f2e7ddd0b98f214ae6
R.T Projects <135464651+RTCreations@users.noreply.github.com>
2026-07-15 19:04:22 -0500

    Added further scaling and designed the website

--- docs/js/upgrades.js
+++ docs/js/upgrades.js
@@ -24,32 +24,32 @@ export const upgrades = {
 
     buyEnergyAmplifier() {
         if (player.energy.gte(this.energyAmplifier.cost)) {
-            player.energy = player.energy.minus(this.cost);
+            player.energy = player.energy.minus(this.energyAmplifier.cost);
             this.energyAmplifier.level++;
             player.boughtUpgrades = player.boughtUpgrades.plus(1);
             player.energyPerSecond = player.energyPerSecond
             .times(2);
-            this.energyAmplifier.cost = new Decimal(this.energyAmplifier.cost).times(3);
+            this.energyAmplifier.cost = new Decimal(this.energyAmplifier.cost).times(4).pow(1.01);
         }
     },
 
     buyEnergyBoost() {
         if (player.energy.gte(this.energyBoost.cost)) {
-            player.energy = player.energy.minus(this.cost);
+            player.energy = player.energy.minus(this.energyBoost.cost);
             this.energyBoost.level++;
             player.boughtUpgrades = player.boughtUpgrades.plus(1);
-            player.energyPerSecond = player.energyPerSecond.times(3);
+            player.energyPerSecond = player.energyPerSecond.times(2);
             this.energyBoost.cost = new Decimal(this.energyBoost.cost).times(5);
         }
     },
 
     buyEnergyAccelerate() {
             if (player.energy.gte(this.energyAccelerate.cost)) {
-                player.energy = player.energy.minus(this.cost);
+                player.energy = player.energy.minus(this.energyAccelerate.cost);
                 this.energyAccelerate.level++;
                 player.boughtUpgrades = player.boughtUpgrades.plus(1);
                 speedUp();
-                this.energyAccelerate.cost = new Decimal(this.energyAccelerate.cost).times(10);
+                this.energyAccelerate.cost = new Decimal(this.energyAccelerate.cost).times(15);
             }
         }
 };
