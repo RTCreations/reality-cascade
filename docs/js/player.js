a7f4f71c51b6ad658364a085a924a49d6f07be04
R.T Projects <135464651+RTCreations@users.noreply.github.com>
2026-08-19 22:08:30 -0500

    Fixed testing player default stats

--- docs/js/player.js
+++ docs/js/player.js
@@ -5,9 +5,9 @@ import { upgrades } from "./upgrades.js";
 export const player = {
     primon: new Decimal("0"),
     primonsPerSecond: new Decimal("1"),
-    primonSpeed: 50,
+    primonSpeed: 500,
     primonMultiplier: new Decimal("1"),
-    primonAchievementBonus: new Decimal("100"),
+    primonAchievementBonus: new Decimal("1"),
     autoBuyPrimon: false,
 
     antiEnergy: new Decimal("0"),
